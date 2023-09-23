661e97a5012be14f5ff7d82c4b39a356b61389e6
Alejandro <alejandro.atala@municipiodetandil.edu.ar>
2023-09-23 13:32:38 -0300

    formato al mail

--- src/twilio/twilio.service.ts
+++ src/twilio/twilio.service.ts
@@ -42,14 +42,32 @@ const transporter = nodemailer.createTransport({
   }
 });
 
+
+
+
+
 export class TwilioService {
   async sendEmail(to: string, subject: string, text: string): Promise<void> {
+
+
+    const emailContent = `
+  <div style="background-color:rgba(78, 202, 155, 0.618); padding: 20px;">
+    <div style="background-color: white; border-radius: 10px; padding: 20px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);">
+      <h2 style="color: #333;">Reserva de turno</h2>
+      <p>${text}</p>
+
+      <p>Gracias por su reserva.</p>
+    </div>
+  </div>
+`;
+
+
     console.log('enviando mail...')
     const mailOptions = {
       from: 'aledorrego89@gmail.com',
       to,
       subject,
-      text
+      html: emailContent
     };
 console.log(mailOptions);
     await transporter.sendMail(mailOptions);
