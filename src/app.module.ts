6964bbc2b7b035035f3f79c7322f47343a6c85e3
Alejandro <104107373+alejandro-atala@users.noreply.github.com>
2023-10-04 20:47:10 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,11 +11,11 @@ import { ValoresModule } from './valores/valores.module';
   imports: [
     TypeOrmModule.forRoot({
       type: "mysql",
-      host: "bm0yetkr3k6vu9rarje4-mysql.services.clever-cloud.com",
+      host: "b2ntwrjekadzfu9hb7ir-mysql.services.clever-cloud.com",
       port: 3306,
-      username: "uncahqiymeja6uj2",
-      password: "gs32FqABxC2Rz7D39j4w",
-      database: "bm0yetkr3k6vu9rarje4",
+      username: "u6x5wjkxdrm6jusr",
+      password: "P6lnT6akABFVw4Xb28XV",
+      database: "b2ntwrjekadzfu9hb7ir",
       entities: [
         "dist/**/**.entity{.ts,.js}",
       ],
